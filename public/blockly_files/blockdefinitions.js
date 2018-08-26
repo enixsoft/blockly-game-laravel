0de571cf6748c00b3a5bf95f80140cbef371e7da
Martin Vančo <enixsoft@users.noreply.github.com>
2018-08-26 20:48:03 +0200

    0.09
    blocks changes, additions

--- public/blockly_files/blockdefinitions.js
+++ public/blockly_files/blockdefinitions.js
@@ -4,7 +4,7 @@ Blockly.Blocks['move_right'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("move right")
-        .appendField(new Blockly.FieldImage("https://png.icons8.com/material/1600/right.png", 15, 15, "*"));
+        .appendField(new Blockly.FieldImage("https://png.icons8.com/material/1600/right.png", 20, 20, "*"));
     this.setPreviousStatement(true, "Action");
     this.setNextStatement(true, "Action");
     this.setColour(160);
@@ -13,11 +13,12 @@ Blockly.Blocks['move_right'] = {
   }
 };
 
+
 Blockly.Blocks['move_left'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("move left")
-        .appendField(new Blockly.FieldImage("https://png.icons8.com/material/1600/left.png", 15, 15, "*"));
+        .appendField(new Blockly.FieldImage("https://png.icons8.com/material/1600/left.png", 20, 20, "*"));
     this.setPreviousStatement(true, "Action");
     this.setNextStatement(true, "Action");
     this.setColour(160);
@@ -26,6 +27,45 @@ Blockly.Blocks['move_left'] = {
   }
 };
 
+Blockly.Blocks['move_up'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("move up")
+        .appendField(new Blockly.FieldImage("https://png.icons8.com/material/1600/up.png", 20, 20, "*"));       
+    this.setPreviousStatement(true, "Action");
+    this.setNextStatement(true, "Action");
+    this.setColour(160);
+ this.setTooltip("Move the character up");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['move_down'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("move down")
+        .appendField(new Blockly.FieldImage("https://png.icons8.com/material/1600/down.png", 20, 20, "*"));
+    this.setPreviousStatement(true, "Action");
+    this.setNextStatement(true, "Action");
+    this.setColour(160);
+ this.setTooltip("Move the character down");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['attack'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("attack")
+        .appendField(new Blockly.FieldImage("https://png.icons8.com/color/sword.png", 20, 20, "*"))
+        .appendField(new Blockly.FieldDropdown([["right","right"], ["left","left"], ["up","up"], ["down","down"]]), "direction");
+    this.setPreviousStatement(true, "Action");
+    this.setNextStatement(true, "Action");
+    this.setColour(160);
+ this.setTooltip("Character attacks");
+ this.setHelpUrl("");
+  }
+};
 
 Blockly.Blocks['player'] = {
   init: function() {
@@ -48,6 +88,25 @@ Blockly.JavaScript['move_left'] = function(block) {
 Blockly.JavaScript['move_right'] = function(block) {
   // TODO: Assemble JavaScript into code variable.	
   var code = 'moveRight();\n';
+  return code;
+};
+
+Blockly.JavaScript['move_up'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.	
+  var code = 'moveUp();\n';
+  return code;
+};
+
+Blockly.JavaScript['move_down'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.	
+  var code = 'moveDown();\n';
+  return code;
+};
+
+Blockly.JavaScript['attack'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var dropdown_direction = block.getFieldValue('direction');	
+  var code = 'attack(' + dropdown_direction + ');\n';
   return code;
 };
 
